2ed0d0125a7c8952bf70fd6db6100853c079ea89
Alex <alexandra.bulatova95@mail.ru>
2026-01-13 11:35:52 +0500

    ---

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        var v = "String";
+        Integer I = 25;
+        int j = 25;
+
+        final int fif = 10;
+
+        boolean isAlive = true;
+        byte b = -128 + 127;
+        short s = -32768 + 32767;
+        int ia = -2147483648 + 2147483647;
+        long l = -9223372036854775808l + 9223372036854775807l;
+        float f1 = 8.5F;
+        double d1 = 8.5;
+
+        Scanner scannerIn = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String userName = scannerIn.nextLine();
+
+        int[] arr1;
+    }
+}
